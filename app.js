1eff65cd04862801bbf8605579811ac719fd20d3
brandon <github@thequinn.org>
2023-06-14 10:37:24 -0500

    cleaning up js

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ let password = ''
 
 passwordInput.addEventListener('change', function(e) {
     password = e.target.value;
-    console.log(password);
-    return password
 })
 
 confirmPasswordInput.addEventListener('input', function(e) {
@@ -18,13 +16,11 @@ function checkPassword(password,confirmPassword) {
     if(password === confirmPassword){
         passwordInput.classList.remove('invalid');
         confirmPasswordInput.classList.remove('invalid');
-        console.log('true');
         return true;
     }
     else{
         passwordInput.classList.add('invalid');
         confirmPasswordInput.classList.add('invalid');
-        console.log('false')
         return false;
     }
 }
